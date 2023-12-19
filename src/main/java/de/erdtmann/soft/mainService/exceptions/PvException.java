0741f88c920685ff2e58ee29df1dbb87eb2a00dc
Frank Erdtmann <erdtmann.frank@gmail.com>
2023-12-19 23:49:19 +0100

    PvException gespeichert

---
+++ src/main/java/de/erdtmann/soft/mainService/exceptions/PvException.java
@@ -0,0 +1,12 @@
+package de.erdtmann.soft.mainService.exceptions;
+
+public class PvException extends Exception {
+
+	private static final long serialVersionUID = 7645909342126646834L;
+
+	public PvException() {}
+	
+	public PvException(String message) {
+		super(message);
+	}
+}
