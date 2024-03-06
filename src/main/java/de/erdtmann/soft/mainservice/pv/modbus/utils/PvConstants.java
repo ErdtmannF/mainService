e27b5c6b2c0aa7bbda8e3bcf32283051b1e69946
Frank Erdtmann <erdtmann.frank@gmail.com>
2024-03-06 23:17:12 +0100

    Wechselrichter IP in Namen geändert

--- src/main/java/de/erdtmann/soft/mainservice/pv/modbus/utils/PvConstants.java
+++ src/main/java/de/erdtmann/soft/mainservice/pv/modbus/utils/PvConstants.java
@@ -4,7 +4,7 @@ package de.erdtmann.soft.mainservice.pv.modbus.utils;
 
 public final class PvConstants {
 	
-	public static final String INVERTER_URL = "192.168.178.71";
+	public static final String INVERTER_URL = "erdtmannpv";
 	public static final int INVERTER_PORT = 1502;
 	public static final int INVERTER_UNIT_ID = 71;
 
